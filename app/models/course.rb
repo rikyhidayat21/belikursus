4f0bea0c1514369bf75ac3f6c5f4149b838e0c8c
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-08-22 21:28:52 +0700

    BK-5: add validation for course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,5 @@
 class Course < ApplicationRecord
+  validates :title, :description, :image_url, :price, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: { greater_than_or_equal_to: 1 }
 end
