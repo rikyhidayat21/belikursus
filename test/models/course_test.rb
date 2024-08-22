36f12d7b4043be4951ba1635c4925dcb16d75ba7
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-08-22 21:59:17 +0700

    BK-5: add test for course model

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "course attributes should not be empty" do
+    course = Course.new
+    assert course.invalid?
+    assert course.errors[:title].any?
+    assert course.errors[:description].any?
+    assert course.errors[:image_url].any?
+    assert course.errors[:price].any?
+  end
+
+  test "price must be positive" do
+    course = Course.new(title: "Testing model", description: "model desc", image_url: "model.png")
+
+    course.price = -1
+    assert course.invalid?
+    assert_equal([ "must be greater than or equal to 1" ], course.errors[:price])
+
+    course.price = 1
+    assert course.valid?
+  end
+
+  # add test for image_url, only accept png/jpg/jpeg
 end
