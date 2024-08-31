930242e9780fd75e60b6ce95fd8983a8bc6515e4
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-08-31 15:02:34 +0700

    BK-9: add title minimum chars validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
   validates :title, :description, :image_url, :price, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { minimum: 10 }
   validates :price, numericality: { greater_than_or_equal_to: 1 }
 end
