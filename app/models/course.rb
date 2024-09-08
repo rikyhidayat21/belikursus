745a3ae7bac012d8294a85703303a826a7fdeebb
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-09-08 21:10:58 +0700

    BK-16: ensure course cannot be deleted if refenced

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,18 @@
 class Course < ApplicationRecord
   has_many :line_items
 
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, :price, presence: true
   validates :title, uniqueness: true, length: { minimum: 10 }
   validates :price, numericality: { greater_than_or_equal_to: 1 }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, "Line Items present")
+      throw :abort
+    end
+  end
 end
