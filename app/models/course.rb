73ba8bc68824f946a39a4d5b3af5f0039ef05348
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-09-08 21:04:51 +0700

    BK-16: generate line items & adjust model association

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+  has_many :line_items
+
   validates :title, :description, :image_url, :price, presence: true
   validates :title, uniqueness: true, length: { minimum: 10 }
   validates :price, numericality: { greater_than_or_equal_to: 1 }
